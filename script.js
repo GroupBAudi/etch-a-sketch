a56397a9aebdcfbcc46c12d3acec59d0b402216c
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-04-18 07:12:21 +0700

    Adds random color change when the box are hovered

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let div = "";
 let divRow = "";
 for (let j = 1; j <= 16; j++) {
     div += `<div class="box"></div>`
-    console.log(j);
 } 
 for (let i = 1; i <= 16; i++) {
     divRow += `<div class="grid-row">${div}</div>`  
@@ -18,20 +17,34 @@ container.innerHTML = divRow;
     string first and set innerHTML once
 */
 
+let random = (number) => {
+    return Math.floor(Math.random() * (number + 1))
+}
+
+const boxHover = document.querySelectorAll(".box");
+boxHover.forEach((element) => {
+    element.addEventListener("mouseenter", (event) => {
+        const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        element.style.backgroundColor = randomColor;
+    })
+})
+
 const newGrid = document.querySelector(".newGrid"); // selects the button
 newGrid.addEventListener("click", (event) => {  // adds function for creating new x by x grid, x is represented by newGrid
         let newGrid = parseInt(prompt("How many grid? (Maximum of 100 blin)"));
-        let div = ""; 
-        let divRow = "";
-        for (let j = 1; j <= newGrid; j++) {
-            div += `<div class="box"></div>`
-        }
-        for (let i = 1; i <= newGrid; i++) {
-            divRow += `<div class="grid-row">${div}</div>` 
-        }
-        container.innerHTML = divRow;
+        if (newGrid > 100) {
+            console.error("Can't. Muh pc can't handle this 😭")
+        } else {
+            let div = ""; 
+            let divRow = "";
+            for (let j = 1; j <= newGrid; j++) {
+                div += `<div class="box"></div>`
+            }
+            for (let i = 1; i <= newGrid; i++) {
+                divRow += `<div class="grid-row">${div}</div>` 
+            }
+            container.innerHTML = divRow;
+        }       
 })
-
-
 
 
