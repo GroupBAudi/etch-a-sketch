fc17920a8aa84e4ba6f882f8e61e81d017d45533
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-04-19 06:03:33 +0700

    Adds random color generator

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 // Initial setup
-let div = ""; 
-let divRow = "";
-for (let j = 1; j <= 16; j++) {
-    div += `<div class="box"></div>`
-} 
-for (let i = 1; i <= 16; i++) {
-    divRow += `<div class="grid-row">${div}</div>`  
+const container = document.querySelector(".container");
+
+function grid (count) {
+    for (let i = 0; i < count; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "grid-row");
+        container.appendChild(row);
+        for (let j = 0 ; j < count; j++) {
+            const box = document.createElement("div");
+            box.setAttribute("class", "box");
+            row.appendChild(box);
+        }
+    }
 }
 
-const container = document.querySelector(".container");
-container.innerHTML = divRow;
+grid(16);
 
-/* Apparently each time innerHTML is set, it's parsed repeatedly.
-    Essentially, if we modify innerHTML in a loop, DOM is discarded and markup
-    is re-parsed for every iteration. Therefore, we build the entire markup as a 
-    string first and set innerHTML once
-*/
+// random color generator
 
 let random = (number) => {
     return Math.floor(Math.random() * (number + 1))
 }
 
-const boxHover = document.querySelectorAll(".box");
-boxHover.forEach((element) => {
-    element.addEventListener("mouseenter", (event) => {
-        const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-        element.style.backgroundColor = randomColor;
-    })
-})
+
 
 const newGrid = document.querySelector(".newGrid"); // selects the button
 newGrid.addEventListener("click", (event) => {  // adds function for creating new x by x grid, x is represented by newGrid
         let newGrid = parseInt(prompt("How many grid? (Maximum of 100 blin)"));
+        container.querySelectorAll(".grid-row").forEach((element) => {
+            element.remove();
+        })
         if (newGrid > 100) {
             console.error("Can't. Muh pc can't handle this 😭")
         } else {
-            let div = ""; 
-            let divRow = "";
-            for (let j = 1; j <= newGrid; j++) {
-                div += `<div class="box"></div>`
+        for (let i = 0; i < newGrid; i++) {
+            const row = document.createElement("div");
+            row.setAttribute("class", "grid-row");
+            container.appendChild(row);
+            for (let j = 0 ; j < newGrid; j++) {
+                const box = document.createElement("div");
+                box.setAttribute("class", "box");
+                row.appendChild(box);
             }
-            for (let i = 1; i <= newGrid; i++) {
-                divRow += `<div class="grid-row">${div}</div>` 
-            }
-            container.innerHTML = divRow;
-        }       
+            
+        }
+        const boxHover = document.querySelectorAll(".box");
+        boxHover.forEach((element) => {
+            element.addEventListener("mouseover", (event) => {
+                const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+                element.style.backgroundColor = randomColor;
+            })
+        })
+
+    }       
 })
 
-
+const boxHover = document.querySelectorAll(".box");
+boxHover.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+        const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        element.style.backgroundColor = randomColor;
+    })
+})
